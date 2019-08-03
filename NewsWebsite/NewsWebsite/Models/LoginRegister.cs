60a2a4fa4e47f767de7837a7b0db2d2cc3391798
Daniel Paz <danielpaz601@gmail.com>
2019-08-03 20:55:38 +0300

    Added the design to the login system

---
+++ NewsWebsite/NewsWebsite/Models/LoginRegister.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsWebsite.Models
+{
+    public class LoginRegister
+    {
+        public NewsWebsite.Models.RegisterViewModel Item1 { get; set; }
+        public NewsWebsite.Models.LoginViewModel Item2 { get; set; }
+    }
+}
